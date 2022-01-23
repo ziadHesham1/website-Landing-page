d2be3f27f1851d59e031119ba1e1b74a337b4733
ziadHesham1 <71788152+ziadHesham1@users.noreply.github.com>
2022-01-23 23:04:53 +0200

    final changes

--- js/app.js
+++ js/app.js
@@ -2,27 +2,26 @@
 let menu = document.getElementById('navbar__list');
 
 // create variable holds all current section
-let allSections = Array.from(document.querySelectorAll('section'));
+let sectionsArray = Array.from(document.querySelectorAll('section'));
 
 
-let TAB_ACTIVE_CLASS = 'your-active-tab';
-let SECTION_ACTIVE_CLASS = 'your-active-class';
-let DATA_ATTRIBUTE = 'data-link';
-
-
-// add tabs of current sections
-//------------------------------
+let TAB_ACTIVE_CLASS = "your-active-tab";
+let SECTION_ACTIVE_CLASS = "your-active-class";
+let DATA_ATTRIBUTE = "data-link";
 
 
 /**
- * returns anchor element with href and text content
- * to append inside the new tab
+ * create anchor element with href and text content
+ * @param {number} i the number of section currently creating
+ * @return {HTMLAnchorElement}link
  */
 function createAnchor(i) {
     // create anchor as child to li (a)
-    let link = document.createElement('a');
+    let link = document.createElement("a");
     // momkn a8erha w3ml elly felvideo
     // set a href to it's section id
+    link.className = "menu__link";
+
     link.setAttribute(DATA_ATTRIBUTE, `section${i}`);
     // set tab text to section + i
     link.textContent = `Section ${i}`;
@@ -31,15 +30,14 @@ function createAnchor(i) {
 
 
 /**
- * returns li element and append the anchor inside it
- * to append it inside the menu(ul)
-
+ * create li element and append the anchor inside it
+ * @param {number} i the number of section currently creating
+ * @return {HTMLLIElement} newTab
  */
 function createTab(i) {
     // create new tab (li)
-    let newTab = document.createElement('li');
+    let newTab = document.createElement("li");
     // set new tab class
-    newTab.className = 'menu__link';
 
     //append link to new tab
     newTab.appendChild(createAnchor(i));
@@ -47,50 +45,38 @@ function createTab(i) {
     return newTab;
 }
 
-/**
- * loop in range of sections number
- *  append created tabs to the ul
- *  every current section have corresponding tab
- */
-for (let i = 1; i <= allSections.length; i++) {
-    // append new tab to ul
-    menu.appendChild(createTab(i));
-}
-//===================================================================================
+// append created tabs to the ul for each existing section
+sectionsArray.forEach((val, i) => menu.appendChild(createTab(i + 1)));
 
-//declare variables
-let menuTabs = Array.from(document.querySelectorAll('.menu__link'));
-
-// let menuTabLinks = Array.from(document.querySelectorAll('.menu__link a'));
-//=================================================
-
-/*
-* Now we need to click one of the menu tabs and it takes me to its corresponding section
-* and change clicked tab style to active mode
-* */
+// create variable holds created li elements
+let menuTabs = document.querySelectorAll("#navbar__list li");
 
 
 /*
 *this function will take the clicked tab as an argument
 * then scroll to this tab corresponding section
 **/
+
+
+/**
+ * scroll to clicked tab corresponding section
+ * @param clickedTab
+ */
 function scrollToSection(clickedTab) {
     let TAB_DATA_ATTRIBUTE = clickedTab.firstChild.getAttribute(DATA_ATTRIBUTE);
     let destinationSection = document.getElementById(TAB_DATA_ATTRIBUTE);
-    destinationSection.scrollIntoView({behavior: 'smooth', block: 'start'});
-
+    destinationSection.scrollIntoView({behavior: "smooth", block: "start"});
 }
 
-/*
-*this function will take the clicked tab as an argument
-* then adds TAB_ACTIVE_CLASS to its class list to have active style 
-**/
 
+/**
+ * add TAB_ACTIVE_CLASS to clicked tab class list to have active state
+ * @param clickedTab
+ */
 function toggleActiveTabByClick(clickedTab) {
     menuTabs.forEach((menuTab) => {
         menuTab.classList.remove(TAB_ACTIVE_CLASS);
     });
-    console.log(clickedTab);
 
     clickedTab.classList.add(TAB_ACTIVE_CLASS);
 
@@ -102,7 +88,7 @@ function toggleActiveTabByClick(clickedTab) {
 * when there is a tab clicked it just pass this tab to toggleActiveTab and scrollToSection functions
 * */
 menuTabs.forEach(tab => {
-    tab.addEventListener('click', (clickedTab) => {
+    tab.addEventListener("click", (clickedTab) => {
         clickedTab.preventDefault();
         scrollToSection(clickedTab.currentTarget);
         toggleActiveTabByClick(clickedTab.currentTarget);
@@ -110,6 +96,10 @@ menuTabs.forEach(tab => {
 });
 
 
+/**
+ * add active state for the tab corresponding to the section in the viewport
+ * @param activeSection
+ */
 function toggleActiveTabByScroll(activeSection) {
 
     let sectionId = activeSection.id;
@@ -126,36 +116,72 @@ function toggleActiveTabByScroll(activeSection) {
 }
 
 
-// set active theme to the visible section when scrolling
-// and when one of the sections is visible on screen it should change its style to active mode
-function adjustActiveClass(elementsList) {
-    const options = {
-        threshold: .5
-    };
-    const observer = new
-    IntersectionObserver(
-        function (entries
-            , observer) {
+/**
+ * set active state to the the section in the viewport
+ * pass the section in the viewport to toggleActiveTabByScroll function
+ * @param entries
+ */
 
-            entries.forEach((item) => {
-                if (item.isIntersecting) {
-                    item.target.classList.add(SECTION_ACTIVE_CLASS);
-                    toggleActiveTabByScroll(item.target);
-                } else {
-                    item.target.classList.remove(SECTION_ACTIVE_CLASS);
-                }
-            })
-        }, options);
+function observerAction(entries) {
 
-    elementsList.forEach((item) => {
-        observer.observe(item)
-    });
+    entries.forEach((item) => {
+        if (item.isIntersecting) {
+            item.target.classList.add(SECTION_ACTIVE_CLASS);
+            toggleActiveTabByScroll(item.target);
+        } else {
+            item.target.classList.remove(SECTION_ACTIVE_CLASS);
+        }
+    })
 }
 
-adjustActiveClass(allSections);
+const observer = new IntersectionObserver(observerAction, {threshold: .5});
 
-//=================================================
-
-//functions calling
+sectionsArray.forEach(item => observer.observe(item));
 
 
+//trying something
+let newIcon = document.querySelector('.toggle-menu');
+let myass = document.querySelector('.navbar__menu ul');
+let myassli = document.querySelectorAll('.navbar__menu ul li');
+
+function toggleVisibility(x) {
+
+    if (x.style.display === "none") {
+
+        x.style.display = "block";
+
+    } else {
+        x.style.display = "none";
+    }
+}
+
+/*body.onclick = function (){
+    
+    toggleVisibility(menu)
+}*/
+newIcon.onclick = function () {
+    console.log(`yes click me`);
+    toggleVisibility(menu)
+}
+
+
+//------------------------------------------------------------
+
+
+/*what next:
+* try using data-nav to add <a> text 
+* edit menu style for small screens (check udacity tips)
+* try scroll to top button
+* add some comments
+* complete readme file
+* reorganize code if possible
+* try active tab when scrolling(done)
+* check viewPort other method(done)
+* check scroll other method (s3at bdos wmyt7rksh) Talbha fel rubricK (done)
+* */
+
+/*
+* ERRORS
+* there is an error when loading page in the console
+* another one appear when tried in edge
+* */
